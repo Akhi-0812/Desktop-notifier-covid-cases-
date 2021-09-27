957e02df7c156ad0c954c5de51e1d5dde8c66881
Akhi-0812 <73642930+Akhi-0812@users.noreply.github.com>
2021-09-27 19:54:07 +0530

    Add files via upload

---
+++ covi.py
@@ -0,0 +1,22 @@
+import datetime
+import time 
+import requests
+from plyer import notification
+covidData = None
+try:
+    covidData = requests.get("https://corona-rest-api.herokuapp.com/Api/india")
+except:
+    print("Please! Check your internet connection")
+if (covidData != None):
+    data = covidData.json()['Success']
+    while(True):
+        notification.notify(
+            title = "COVID-19 Stats in India",
+            message = "Total cases reported : {totalcases}\nCases reported today : {todaycases}\nDeaths reported : {todaydeaths}\nTotal active cases : {active}".format(
+                        totalcases = data['cases'],
+                        todaycases = data['todayCases'],
+                        todaydeaths = data['todayDeaths'],
+                        active = data["active"]),app_icon =  "E:\\coronavirus.ico",
+            timeout  = 100
+        )
+        time.sleep(60*60*4)
